28552bc19acc33276f0fc30f7b5934611de6f89d
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-01-30 19:39:09 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -79,11 +79,22 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+    
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
     libkeymaster41 \
-    libpuresoftkeymasterdevice
+    libpuresoftkeymasterdevice \
+    libion \
+    libxml2
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
